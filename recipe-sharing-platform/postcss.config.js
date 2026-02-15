2e5104293d8239b70b2f1a1acf09cdff0f5a0548
Ampaabeng Kyeremeh Nancy <kampaabeng01@gmail.com>
2026-02-15 23:10:27 +0000

    fix: install tailwind dependencies and add configuration files

---
+++ recipe-sharing-platform/postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: c:\Users\PIWC_TESANO\Desktop\FE\HTML\alx-fe-reactjs\recipe-sharing-platform\postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
